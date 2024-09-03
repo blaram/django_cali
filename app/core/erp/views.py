be32665c9af9d229db8b179bd02dea1bdc23dde5
Bladimir Ramos <bladi.ram@gmail.com>
2024-09-02 22:27:56 -0400

    Configuramos los templates

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+from core.erp.models import Category, Product
+
+
 # Create your views here.
 def myfirstview(request):
     data = {
-        'name': 'Blas'
+        'name': 'Blas',
+        'categories': Category.objects.all()
     }
-    return JsonResponse(data)
+    return render(request, 'index.html', data)
+
+def mysecondview(request):
+    data = {
+        'name': 'Blas',
+        'categories': Category.objects.all(),
+        'products': Product.objects.all()
+    }
+    return render(request, 'second.html', data)
